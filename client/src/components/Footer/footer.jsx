8b2bece4eb96cf26b9f1fcda4619f3994a954708
achiyat <achiya308@gmail.com>
2024-08-26 04:40:25 +0300

    footer-Responsivenes

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -13,42 +13,44 @@ export const Footer = () => {
   return (
     <section className="footer-section" id="footer">
       <div className="footer-up">
-        <div className="footer-list">
-          <h3 className="footer-title">About Us</h3>
-          <p>{renderSpan("About Movie App")}</p>
-          <p>{renderSpan("Contact Us")}</p>
-          <p>{renderSpan("Terms of Service")}</p>
-          <p>{renderSpan("Private Policy")}</p>
-        </div>
-        <div className="footer-list">
-          <h3 className="footer-title">Top Movies</h3>
-          <Link to={`/genre/${28}`}>
-            <p>{renderSpan("Action")}</p>
-          </Link>
-          <Link to={`/genre/${10749}`}>
-            <p>{renderSpan("Romance")}</p>
-          </Link>
-          <Link to={`/genre/${35}`}>
-            <p>{renderSpan("Comedy")}</p>
-          </Link>
-          <Link to={`/genre/${27}`}>
-            <p>{renderSpan("Horror")}</p>
-          </Link>
-        </div>
-        <div className="footer-list">
-          <h3 className="footer-title">Genres</h3>
-          <Link to={`/genre/${12}`}>
-            <p>{renderSpan("Adventure")}</p>
-          </Link>
-          <Link to={`/genre/${80}`}>
-            <p>{renderSpan("Crime")}</p>
-          </Link>
-          <Link to={`/genre/${14}`}>
-            <p>{renderSpan("Fantasy")}</p>
-          </Link>
-          <Link to={`/genre/${16}`}>
-            <p>{renderSpan("Animation")}</p>
-          </Link>
+        <div className="footer-lists">
+          <div className="footer-list">
+            <h3 className="footer-title">Top Movies</h3>
+            <Link to={`/genre/28`}>
+              <p>{renderSpan("Action")}</p>
+            </Link>
+            <Link to={`/genre/10749`}>
+              <p>{renderSpan("Romance")}</p>
+            </Link>
+            <Link to={`/genre/35`}>
+              <p>{renderSpan("Comedy")}</p>
+            </Link>
+            <Link to={`/genre/27`}>
+              <p>{renderSpan("Horror")}</p>
+            </Link>
+          </div>
+          <div className="footer-list">
+            <h3 className="footer-title">Genres</h3>
+            <Link to={`/genre/12`}>
+              <p>{renderSpan("Adventure")}</p>
+            </Link>
+            <Link to={`/genre/80`}>
+              <p>{renderSpan("Crime")}</p>
+            </Link>
+            <Link to={`/genre/14`}>
+              <p>{renderSpan("Fantasy")}</p>
+            </Link>
+            <Link to={`/genre/16`}>
+              <p>{renderSpan("Animation")}</p>
+            </Link>
+          </div>
+          <div className="footer-list">
+            <h3 className="footer-title">About Us</h3>
+            <p>{renderSpan("About Movie App")}</p>
+            <p>{renderSpan("Contact Us")}</p>
+            <p>{renderSpan("Terms of Service")}</p>
+            <p>{renderSpan("Private Policy")}</p>
+          </div>
         </div>
         <div className="footer-subscribe">
           <h3 className="footer-title">Subscribe</h3>
