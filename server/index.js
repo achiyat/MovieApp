e84f8bf48cdf04d74b7f2c09fd848494459be8b3
achiyat <achiya308@gmail.com>
2024-09-24 17:34:37 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const API_KEY = process.env.TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 const clientPath = fileURLToPath(
-  new URL("../../client/build", import.meta.url)
+  new URL("../../src/client/build", import.meta.url)
 );
 
 app.use(express.static(clientPath));
